c927fd0b95e4c9a8737c7937d387726c95c8f62a
Santosh Bhattarai <hsotnas76@gmail.com>
2022-03-01 21:52:11 +0545

    authenticate screen ui update,add assets

--- lib/screens/auth/authenticate.dart
+++ lib/screens/auth/authenticate.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'login.dart';
-import 'register.dart';
-
+import '../../utils/constants.dart';
 
 class Authenticate extends StatefulWidget {
   const Authenticate({Key? key}) : super(key: key);
@@ -12,18 +10,78 @@ class Authenticate extends StatefulWidget {
 }
 
 class _AuthenticateState extends State<Authenticate> {
-  bool showLogin = true;
-
-  void toggleView() {
-    setState(() => showLogin = !showLogin);
-  }
-
   @override
   Widget build(BuildContext context) {
-    if (showLogin) {
-      return Login(toggleView: toggleView);
-    } else {
-      return Register(toggleView: toggleView);
-    }
+    return Scaffold(
+      backgroundColor: Constants.primaryColor,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                  Container(
+                    height: 150.0,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/cloth_faded.png'),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Image.asset(
+                      'assets/images/illustration.png',
+                      scale: 1.1,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 24.0,
+                ),
+                decoration: const BoxDecoration(
+                  color: Constants.scaffoldBackgroundColor,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30.0),
+                    topLeft: Radius.circular(30.0),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    SizedBox(height: 20.0),
+                    Text(
+                      'Welcome to Laundree!',
+                    ),
+
+                    Text(
+                      'This is the first version of our laundry app. Please sign in or create an account below.',
+                      style: TextStyle(
+                        color: Color.fromRGBO(74, 77, 84, 1),
+                        fontSize: 14.0,
+                      ),
+                    ),
+
+                    // Let's create a generic button widget
+
+                    SizedBox(height: 20.0),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
