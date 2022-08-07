032aca8ece6811cd3dfd48981af250924c115075
hsotnas76 <hsotnas76@gmail.com>
2022-08-07 21:30:21 +0545

    add logger

---
+++ lib/logger.dart
@@ -0,0 +1,19 @@
+import 'package:logger/logger.dart';
+
+Logger getLogger(Type type) {
+  return Logger(printer: CustomPrinter(type.toString()));
+}
+
+class CustomPrinter extends LogPrinter {
+  final String className;
+
+  CustomPrinter(this.className);
+  @override
+  List<String> log(LogEvent event) {
+    final color = PrettyPrinter.levelColors[event.level];
+    final emoji = PrettyPrinter.levelEmojis[event.level];
+    final message = event.message;
+
+    return [color!('$emoji  $className $message  ')];
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import 'laundry_app.dart';
+import 'package:laundry/laundry_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(
-    const LaundryApp(),
-  );
+  runApp(const LaundryApp());
 }
